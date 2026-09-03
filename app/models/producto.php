063151957b174126383b7a0b3fcaac7a4c58e8e2
luis gomez <luisangelgomezsaavedra28@gmail.com>
2026-09-03 15:39:24 -0500

    Actualizacion 2.0

--- app/models/producto.php
+++ app/models/producto.php
@@ -4,18 +4,16 @@ require_once __DIR__ . "/../../config/Database.php";
 
 class producto {
     private $connection;
-    
+
     public function __construct(){
         $db = new Database();
-        $thhis->connection = $db->connect();
+        $this->connection = $db->connect();
     }
 
     public function getAll(){
-        $sql = "SELCT * FROM productos";
-        
+        $sql = "SELECT * FROM productos";
+
         $consulta = $this->connection->query($sql);
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
-        
-
     }
 }
--- config/Database.php
+++ config/Database.php
@@ -9,21 +9,20 @@ class Database {
     private $connection;
 
     public function __construct(){
-        $env = parse_ini_file (__DIR__ . "/../.env");
+        $env = parse_ini_file(__DIR__ . "/../.env");
 
-        $this->host =$env['DB_HOST'];
-        $thhis->port =$env['DB_PORT'];
-        $this->db_name =$env['DB_DBNAME'];
-        $this->user =$env['DB_USER'];
-        $this->password =$env['DB_PASS'];
+        $this->host = $env['DB_HOST'];
+        $this->port = $env['DB_PORT'];
+        $this->dbname = $env['DB_NAME'];
+        $this->user = $env['DB_USER'];
+        $this->password = $env['DB_PASS'];
     }
 
-    public function connet(){
-        $dsn = "mysql:host=($this->host);port=($this->port);dbname=($this->dbname)";
+    public function connect(){
+        $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname}";
 
-        $this->connection = new PDO (dsn,$this->username,$this->password);
+        $this->connection = new PDO($dsn, $this->user, $this->password);
 
         return $this->connection;
-
     }
 }
