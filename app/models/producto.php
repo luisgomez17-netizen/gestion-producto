7c99a6bd648b8af04ddc089d9d5349c24aae3dfc
luis gomez <luisangelgomezsaavedra28@gmail.com>
2026-09-10 15:36:42 -0500

    Ejercicio getById

--- app/models/producto.php
+++ app/models/producto.php
@@ -10,10 +10,37 @@ class producto {
         $this->connection = $db->connect();
     }
 
-    public function getAll(){
-        $sql = "SELECT * FROM productos";
+    
+   public function getAll(){
+    $sql = "SELECT 
+                productos.nombre AS producto_nombre,
+                productos.precio,
+                productos.categoria AS producto_categoria,
+                productos.id_proveedor,
+                proveedores.nombre AS proveedor_nombre
+            FROM productos
+            INNER JOIN proveedores ON productos.id_proveedor = proveedores.id";
 
-        $consulta = $this->connection->query($sql);
-        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    $consulta = $this->connection->query($sql);
+    return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function getById($id){
+    $sql = "SELECT 
+                productos.id,
+                productos.nombre AS producto_nombre,
+                productos.precio,
+                productos.categoria AS producto_categoria,
+                productos.id_proveedor,
+                proveedores.nombre AS proveedor_nombre
+            FROM productos
+            INNER JOIN proveedores ON productos.id_proveedor = proveedores.id
+            WHERE productos.id = :id";
+
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute([':id' => $id]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
+
--- app/view/producto/index.php
+++ app/view/producto/index.php
@@ -2,17 +2,38 @@
 
 <table border="1">
     <tr>
-        <th>nombre</th>
-        <th>precio</th>
-        <th>categoria</th>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Categoria Producto</th>
+        <th>Proveedor</th>
     </tr>
     <?php foreach ($productos as $producto): ?>
             <tr>
-                <td><?= $producto['nombre'] ?></td>
+                <td><?= $producto['producto_nombre'] ?></td>
                 <td><?= $producto['precio'] ?></td>
-                <td><?= $producto['categoria'] ?></td>
+                <td><?= $producto['producto_categoria'] ?></td>
+                <td><?= $producto['proveedor_nombre'] ?></td>
             </tr>
         <?php endforeach; ?>
+</table>
 
+<h1>Detalle del Producto</h1>
 
+<table border="1">
+    <tr>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Categoria Producto</th>
+        <th>ID Proveedor</th>
+        <th>Proveedor</th>
+    </tr>
+    <?php if (!empty($producto)): ?>
+        <tr>
+            <td><?= $producto['producto_nombre'] ?></td>
+            <td><?= $producto['precio'] ?></td>
+            <td><?= $producto['producto_categoria'] ?></td>
+            <td><?= $producto['id_proveedor'] ?></td>
+            <td><?= $producto['proveedor_nombre'] ?></td>
+        </tr>
+    <?php endif; ?>
 </table>
