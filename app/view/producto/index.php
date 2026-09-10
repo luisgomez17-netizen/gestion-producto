8246e205a7f36d2dc5cac4c545e3e73cbfcb3d14
luis gomez <luisangelgomezsaavedra28@gmail.com>
2026-09-10 17:22:21 -0500

    Integracion de id_categoria

--- app/view/producto/index.php
+++ app/view/producto/index.php
@@ -7,14 +7,14 @@
         <th>Categoria Producto</th>
         <th>Proveedor</th>
     </tr>
-    <?php foreach ($productos as $producto): ?>
-            <tr>
-                <td><?= $producto['producto_nombre'] ?></td>
-                <td><?= $producto['precio'] ?></td>
-                <td><?= $producto['producto_categoria'] ?></td>
-                <td><?= $producto['proveedor_nombre'] ?></td>
-            </tr>
-        <?php endforeach; ?>
+   <?php foreach ($productos as $producto): ?>
+    <tr>
+        <td><?= $producto['producto_nombre'] ?></td>
+        <td><?= $producto['precio'] ?></td>
+        <td><?= $producto['categoria_nombre'] ?></td>
+        <td><?= $producto['proveedor_nombre'] ?></td>
+    </tr>
+    <?php endforeach; ?>
 </table>
 
 <h1>Detalle del Producto</h1>
@@ -23,17 +23,19 @@
     <tr>
         <th>Nombre</th>
         <th>Precio</th>
+        <th>ID Categoría</th>
         <th>Categoria Producto</th>
         <th>ID Proveedor</th>
         <th>Proveedor</th>
     </tr>
     <?php if (!empty($producto)): ?>
-        <tr>
-            <td><?= $producto['producto_nombre'] ?></td>
-            <td><?= $producto['precio'] ?></td>
-            <td><?= $producto['producto_categoria'] ?></td>
-            <td><?= $producto['id_proveedor'] ?></td>
-            <td><?= $producto['proveedor_nombre'] ?></td>
-        </tr>
+    <tr>
+        <td><?= $producto['producto_nombre'] ?></td>
+        <td><?= $producto['precio'] ?></td>
+        <td><?= $producto['id_categoria'] ?></td>
+        <td><?= $producto['categoria_nombre'] ?></td>
+        <td><?= $producto['id_proveedor'] ?></td>
+        <td><?= $producto['proveedor_nombre'] ?></td>
+    </tr>
     <?php endif; ?>
 </table>
